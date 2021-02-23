f94797552ac191fef85237636fbdec20aa629091
mubashirahmedeginie <mubashar.ahmed@egenienext.com>
2021-02-22 21:02:18 -0800

    Removed IP restriction

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
 <?php
-
-if (!function_exists('getUserIP')) {
-
-    function getUserIP()
-    {
-        $ipaddress = '';
-        if (getenv('HTTP_CLIENT_IP'))
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if (getenv('HTTP_X_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if (getenv('HTTP_X_FORWARDED'))
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if (getenv('HTTP_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if (getenv('HTTP_FORWARDED'))
-            $ipaddress = getenv('HTTP_FORWARDED');
-        else if (getenv('REMOTE_ADDR'))
-            $ipaddress = getenv('REMOTE_ADDR');
-        else
-            $ipaddress = 'UNKNOWN';
-        // $ipaddress = '137.0.0.1 127.0.0.1';
-        return strpos($ipaddress, ',') !== FALSE ? explode(',', $ipaddress)[0] : $ipaddress;
-    }
-}
-
-
-if(!in_array(getUserIP(), ['72.255.38.246'])){
-	die('The site is under maintenance.');
-}
 $GLOBALS['BENCHMARKSTARTTIME'] = microtime(true);
 /**
  * CodeIgniter
