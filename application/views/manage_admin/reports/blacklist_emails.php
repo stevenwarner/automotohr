ebf2c0c49a9060d7e21829a926907ab387e2e46a
mubashirahmedeginie <mubashar.ahmed@egenienext.com>
2021-03-12 10:10:25 +0500

    changed text for last updated date

--- application/views/manage_admin/reports/blacklist_emails.php
+++ application/views/manage_admin/reports/blacklist_emails.php
@@ -17,7 +17,7 @@ foreach($jobs as $job){
     $tr .= '    <td>'.$job['email'].'</td>';
     $tr .= '    <td class="text-'.($job['is_bounced'] == 1 ? "success" : "danger" ).'"><strong>'.($job['is_bounced'] == 1 ? "YES" : "NO" ).'<strong></td>';
     $tr .= '    <td class="text-'.($job['is_complaint'] == 1 ? "success" : "danger") .'"><strong>'.($job['is_complaint'] == 1 ? "YES" : "NO") .'<strong></td>';
-    $tr .= '    <td>'.DateTime::createfromformat('Y-m-d H:i:s', $job['created_at'])->format('M d, D Y H:i').'</td>';
+    $tr .= '    <td>'.DateTime::createfromformat('Y-m-d\TH:i:s.uP', $job['note'])->format('M d, D Y H:i').'</td>';
     $tr .= '</tr>';
 } ?>
 <div class="main">
@@ -57,7 +57,7 @@ foreach($jobs as $job){
                                                                 <th>Email</th>
                                                                 <th>Bounce</th>
                                                                 <th>Complaint</th>
-                                                                <th>DateTime</th>
+                                                                <th>Last Updated On</th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
