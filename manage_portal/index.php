a00a19a30c10fd6ab44861908cb7d88dd4126219
mubashirahmedeginie <mubashar.ahmed@egenienext.com>
2021-05-31 18:18:22 +0500

    Added redirector

--- manage_portal/index.php
+++ manage_portal/index.php
@@ -1,4 +1,8 @@
 <?php
+if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO']!= 'https') {
+	$redirect= 'https://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];
+	header("Location:$redirect");
+}
 /**
  * CodeIgniter
  *
