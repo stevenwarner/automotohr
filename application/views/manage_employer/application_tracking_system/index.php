a2a87dc0095e847df20736f57e3006fa37fce161
mubashirahmedeginie <mubashar.ahmed@egenienext.com>
2021-07-28 14:22:08 +0500

    Removed number

--- application/views/manage_employer/application_tracking_system/index.php
+++ application/views/manage_employer/application_tracking_system/index.php
@@ -2871,6 +2871,8 @@
 
             have_all_records = true;
             last_fetched_id  = 0;
+
+            megaOBJ.phone_e16 = megaOBJ.phone_e16.replace(/[^0-9]/g, '');
             //
             if(megaOBJ.phone_e16.toString().length >= 10 && megaOBJ.phone_e16.toString().substr(0, 1) == 1){
                 megaOBJ.phone_e16 = megaOBJ.phone_e16.toString().substr(1, 10);
