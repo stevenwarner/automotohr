7531c26f885d87fa3503ef829b9e1ea09d87dec9
mubashirahmedeginie <mubashar.ahmed@egenienext.com>
2021-09-03 13:30:27 +0500

    Added employee payroll settings view

---
+++ application/views/payroll/profile/employee_profile.php
@@ -0,0 +1,389 @@
+<!--  -->
+<div class="clearfix"></div>
+<!--  -->
+<div class="row">
+    <div class="col-sm-12">
+        <h1 class="csF18 csB9">Payroll Settings</h1>
+    </div>
+</div>
+<!--  -->
+<div class="resp-tab-content resp-tab-content-active ma10">
+    <!-- Employment Status -->
+    <div class="row">
+        <!--  -->
+        <div class="col-sm-12 col-xs-12">
+            <label class="csF16 csB7 csFC1 ma10">
+                Employment Status
+            </label>
+        </div>
+    </div>
+    <!-- Status -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Status
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+                Full Time (30+ Hours Per Week)
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+
+    <!-- Compensation -->
+    <div class="row">
+        <!--  -->
+        <div class="col-sm-12 col-xs-12">
+            <label class="csF16 csB7 csFC1">
+                Compensation
+            </label>
+        </div>
+    </div>
+    <!-- Employee Type -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+            Employee Type
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+                Salary/No overtime
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Wage -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Wage
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+                <b>$85,000.00</b> per <b>Year</b>
+            </p>
+        </div>
+    </div>
+    <!-- Bonus -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Additional Earning
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+                <b>Bonus</b> <br>
+                <b>$1.00</b> per <b>paycheck</b>
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Add Buttons -->
+    <div class="row mb10">
+        <div class="col-sm-3 col-xs-12 text-right"></div>
+        <div class="col-sm-9 col-xs-12">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Add Earning Type
+            </button>
+        </div>
+    </div>
+
+    <!--  -->
+    <!-- Payment Methods -->
+    <div class="row">
+        <!--  -->
+        <div class="col-sm-12 col-xs-12">
+            <label class="csF16 csB7 csFC1 ma10">
+                Payment Methods
+            </label>
+        </div>
+    </div>
+    <!-- Status -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Payment Method
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               Direct Deposit
+            </p>
+        </div>
+    </div>
+    <!-- Bank Account -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                #1: Bank Account
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               Checking Account
+            </p>
+            <p class="csF16">
+               XXXX5485
+            </p>
+            <p class="csF16">
+               Checking
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Bank Account -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                #2: Bank Account
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               Checking Account
+            </p>
+            <p class="csF16">
+               XXXX5485
+            </p>
+            <p class="csF16">
+               Saving
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Add Button -->
+    <div class="row mb10">
+        <div class="col-sm-3 col-xs-12 text-right"></div>
+        <div class="col-sm-9 col-xs-12">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Add Method
+            </button>
+        </div>
+    </div>
+
+    <!-- Federal Taxes -->
+    <div class="row">
+        <!--  -->
+        <div class="col-sm-12 col-xs-12">
+            <label class="csF16 csB7 csFC1 ma10">
+                Federal Taxes
+            </label>
+        </div>
+    </div>
+    <!-- Filing status -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Filing status
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               Single
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Multiple Jobs -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Multiple jobs
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               No
+            </p>
+        </div>
+    </div>
+    <!-- Dependents -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Dependents
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               $0.00
+            </p>
+        </div>
+    </div>
+    <!-- Other income	 -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Other income
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               $0.00
+            </p>
+        </div>
+    </div>
+    <!-- Deductions	 -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Deductions
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               $0.00
+            </p>
+        </div>
+    </div>
+    <!-- Extra withholding		 -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Extra withholding
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               $0.00
+            </p>
+        </div>
+    </div>
+
+    <!-- California Taxes -->
+    <div class="row">
+        <!--  -->
+        <div class="col-sm-12 col-xs-12">
+            <label class="csF16 csB7 csFC1 ma10">
+                California Taxes
+            </label>
+        </div>
+    </div>
+    <!-- Filing status -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Filing status
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               Single
+            </p>
+        </div>
+        <div class="col-sm-2 col-xs-2 text-right">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0 mtn5">
+                <i class="fa fa-edit" aria-hidden="true"></i>&nbsp;Edit
+            </button>
+        </div>
+    </div>
+    <!-- Withholding Allowance -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Withholding Allowance
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               1
+            </p>
+        </div>
+    </div>
+    <!-- Additional Withholding -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                Additional Withholding
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               $0.00
+            </p>
+        </div>
+    </div>
+    <!-- File new hire report -->
+    <div class="row csHoverRow">
+        <!--  -->
+        <div class="col-sm-3 col-xs-12 text-right">
+            <label class="csF16 csB7">
+                File new hire report
+            </label>
+        </div>
+        <div class="col-sm-7 col-xs-12">
+            <p class="csF16">
+               No
+            </p>
+        </div>
+    </div>
+
+    <br>
+    <!-- Add Buttons -->
+    <div class="row mb10">
+        <div class="col-sm-12 col-xs-12">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Add Special Tax Exemption
+            </button>
+        </div>
+    </div>
+    <div class="row mb10">
+        <div class="col-sm-12 col-xs-12">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Add Garnishment or Custom Deduction
+            </button>
+        </div>
+    </div>
+    <div class="row mb10">
+        <div class="col-sm-12 col-xs-12">
+            <button class="btn btn-link csFC1 csF16 csB7 m0 p0">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;Add Recurring Reimbursement
+            </button>
+        </div>
+    </div>
+</div>
