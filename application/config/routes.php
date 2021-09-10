bfd90de6b68a8d3dc01234c13ae0251054b7c882
Mubashir Ahmed <mubashar.ahmed@egenienext.com>
2021-09-10 15:55:50 +0500

    Fixed review details route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1351,7 +1351,7 @@ $route['performance-management/employee/goals/(:num)']['get'] = 'Employee_manage
 $route['performance-management/employee/reviews/(:num)']['get'] = 'Employee_management/employee_reviews/$1';
 $route['performance-management/reviews/all']['get'] = 'Performance_management/all_reviews';
 $route['performance-management/feedbacks/all']['get'] = 'Performance_management/all_feedbacks';
-$route['performance-management/review/detail/(:num)']['get'] = 'Performance_management/review_details/1';
+$route['performance-management/review/detail/(:num)']['get'] = 'Performance_management/review_details/$1';
 //
 $route['get_employee_profile/(:num)']['get'] = 'Employee_management/GetEmployeeProfile/$1';
 $route['get_all_company_employees']['get'] = 'Employee_management/GetAllEmployees';
