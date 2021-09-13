60111a7bad03473949240ae2b28cdc104e53e57e
Mubashir Ahmed <mubashar.ahmed@egenienext.com>
2021-09-13 17:06:09 +0500

    Changed the review content

--- application/views/Performance_management/theme2/help_box.php
+++ application/views/Performance_management/theme2/help_box.php
@@ -2,13 +2,13 @@
 <div class="panel panel-theme">
     <div class="panel-body">
         <p class="csF14 csInfo">
-            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Reviewee</strong><br />The employee against which the reviewee is being conducted.
+            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Reviewee</strong><br />The employee against which the review is being conducted.
         </p>
         <p class="csF14 csInfo">
             <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Reviewer</strong><br />The employee who will provide feedback that is submitted to the reporting manager(s).
         </p>
         <p class="csF14 csInfo">
-            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Manager</strong><br />The reporting manager that will provide a final feedback after reviweing the answers submitted by the reviwers. The feedback submitted by the Manager(s) will show on the employee(s) review dashboard.
+            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Manager</strong><br />The reporting manager that will provide the final feedback after reviewing the answers submitted by the reviewers. The feedback submitted by the Manager(s) will display on the employee(s) review dashboard.
         </p>
     </div>
 </div>
--- application/views/Performance_management/theme2/left_sidebar_feedback.php
+++ application/views/Performance_management/theme2/left_sidebar_feedback.php
@@ -32,15 +32,15 @@
             </h4>
         </div>
         <div class="panel-body">
-            <p class="csF14">
-                <strong>Reviewee</strong><br />The employee against which the reviewee is being conducted.
-            </p>
-            <p class="csF14">
-                <strong>Reviewer</strong><br />The employee who will provide feedback that is submitted to the reporting manager(s).
-            </p>
-            <p class="csF14">
-                <strong>Manager</strong><br />The reporting manager that will provide a final feedback after reviweing the answers submitted by the reviwers. The feedback submitted by the Manager(s) will show on the employee(s) review dashboard.
-            </p>
-        </div>
+        <p class="csF14 csInfo">
+            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Reviewee</strong><br />The employee against which the review is being conducted.
+        </p>
+        <p class="csF14 csInfo">
+            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Reviewer</strong><br />The employee who will provide feedback that is submitted to the reporting manager(s).
+        </p>
+        <p class="csF14 csInfo">
+            <strong><i class="fa fa-info-circle csF16 csB7" aria-hidden="true"></i>&nbsp;Manager</strong><br />The reporting manager that will provide the final feedback after reviewing the answers submitted by the reviewers. The feedback submitted by the Manager(s) will display on the employee(s) review dashboard.
+        </p>
+    </div>
     </div>
 </div>
