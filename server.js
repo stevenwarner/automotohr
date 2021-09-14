5ceebfb7f02b0e183178388ed1be70872c2c8d7b
Mubashir Ahmed <mubashar.ahmed@egenienext.com>
2021-09-14 11:26:14 +0500

    API Server initial structure

--- server.js
+++ server.js
@@ -9,24 +9,22 @@ const cors = require('cors');
 // Include Middleware
 const AUTH = require('./Auth/Index');
 // Include Routes
-const AttendanceRouter = require('./Attendance/Controller');
+
+// Employee Login Route
 const EmployeeRouter = require('./Employee/Controller');
-const CompanyRouter = require('./Company/Controller');
-const LocationRouter = require('./Company/Location/Controller');
-const TimeslotRouter = require('./Company/Timeslot/Controller');
+
 //
 app.use(cors());
 // Parser
 app.use(express.json());
 // Use Routes
-app.use('/attendance', AUTH, AttendanceRouter);
+//
 app.use('/employee', EmployeeRouter);
-app.use('/company', AUTH, CompanyRouter);
-app.use('/company/locations', AUTH, LocationRouter);
-app.use('/company/timeslots', AUTH, TimeslotRouter);
+// Hello URL
+app.get('/ping', (req, res) => { res.send("All systems ready to go :)"); });
 // For unauthorised routes 
-app.get('*', (req, res) => { res.sendStatus(404); })
-    //
+app.get('*', (req, res) => { res.sendStatus(404); });
+//
 app.listen(3000, () => {
-    console.log('Server running');
-})
+    console.log(`Server running on: http://127.0.0.1:3000`);
+});
