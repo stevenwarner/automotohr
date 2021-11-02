1405312a8b6834f4856b6b1cdbed9b37f12138a0
Mubashir Ahmed <mubashar.ahmed@egenienext.com>
2021-11-02 09:41:11 +0100

    Fixed the error_reporting

--- manage_ams/index.php
+++ manage_ams/index.php
@@ -300,6 +300,9 @@ if($_SERVER['HTTP_HOST'] == 'www.automotosocial.com'){
 	ini_set('display_errors', 1);
 }
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 //
 if(!function_exists('getCreds')){
 	function getCreds($index = false){
