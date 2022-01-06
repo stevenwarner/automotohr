ac3086cd71a7d1742f36d06feb07244f01947978
Umar <umarilyas595@gmail.com>
2022-01-06 08:18:39 +0000

    Updated the logic of ENV

--- executive_admin/index.php
+++ executive_admin/index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', preg_match('/staging/i', $_SERVER['HTTP_HOST']) ? "testing" : "production");
 
 /*
  *---------------------------------------------------------------
