e675b131fc7bfd2410ff3f4d4eee0a7be8361036
Umar <umarilyas595@gmail.com>
2022-01-06 08:27:15 +0000

    updated log path

--- manage_ams/application/config/config.php
+++ manage_ams/application/config/config.php
@@ -224,7 +224,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] =  APPPATH.'../../../logs/';
+$config['log_path'] =  APPPATH.'../../../app_logs/';
 
 /*
 |--------------------------------------------------------------------------
