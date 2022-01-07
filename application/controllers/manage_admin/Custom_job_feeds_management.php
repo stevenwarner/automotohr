0d29bac77f5123e4405e6aa96d2ca3c1ba6225ee
HA Shakoor <abdul.shakoor@egenienext.com>
2022-01-07 18:37:02 +0500

    save urls in the file
    07-Jan-2022

--- application/controllers/manage_admin/Custom_job_feeds_management.php
+++ application/controllers/manage_admin/Custom_job_feeds_management.php
@@ -79,10 +79,8 @@ class Custom_job_feeds_management extends Admin_Controller{
                     $insert_data['url'] = base_url().'job_feeds/'.$insert_data['slug'];
                     $insert_data['creator_sid'] = $admin_id;
                     $insert_id = $this->custom_job_feeds_management_model->insert_custom_feed($insert_data);
-                    if ($insert_id){
-                        $this->update_http_url($insert_data);
-                    }
-
+                    // update the url in http_urls file
+                    $this->update_http_url($insert_data);
                     $redirect = $this->input->post('form-submit');
 
                     if($redirect == 'Save and Add More'){
@@ -93,13 +91,6 @@ class Custom_job_feeds_management extends Admin_Controller{
                 break;
             }
         }
-    }
-
-    //
-    public function update_http_url($data_array){
-
-        _e($data_array, true, true);
-
     }
 
     public function edit_job_feed($feed_id)
@@ -140,6 +131,7 @@ class Custom_job_feeds_management extends Admin_Controller{
                     $update_data['type'] = $this->input->post('type');
                     $update_data['url'] = base_url().'job_feeds/'.$update_data['slug'];
                     $this->custom_job_feeds_management_model->update_custom_feed($feed_id, $update_data);
+                    $this->update_http_url($update_data);
                     $this->session->set_flashdata('message', '<b>Success:</b> Feed Updated Successfully');
                     redirect(base_url('manage_admin/custom_job_feeds_management'), 'refresh');
                 break;
@@ -171,6 +163,29 @@ class Custom_job_feeds_management extends Admin_Controller{
             $this->input->post('feedSid')
         );
         echo 'updated';
+    }
+
+    /**
+     * @param $data_array
+     * @return bool
+     */
+    public function update_http_url($data_array)
+    {
+        $file_name = APPPATH.'../http_urls.json';
+        $file = fopen($file_name, 'r');
+        // read data from file
+        $file_data =  fread($file, filesize($file_name));
+        $file_data = json_decode($file_data, true);
+        // save data to file
+        $file_data[$data_array['slug']] = $data_array['url'];
+        // open file to write
+        $file = fopen($file_name, 'w');
+        // write data in file
+        fwrite($file, json_encode($file_data));
+        // close file after saving data
+        fclose($file);
+        // response
+        return true;
     }
 
 }
