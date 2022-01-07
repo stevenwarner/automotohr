71704931da38816f4254f32a684bb0307e4af298
HA Shakoor <abdul.shakoor@egenienext.com>
2022-01-07 13:11:13 +0500

    redirection for http and https
    07-Jan-2022

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+function redirect_traffic()
+{
+    $allowedHTTPUrls = [];
+    $allowedHTTPUrls['automotohr.local/shakoor'] = true;
+    //
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'http';
+    //
+    if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO']!= 'https') {
+        $original_url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+        $url_parts = parse_url($original_url);
+        $url = preg_replace('/^www\./i', '', $url_parts['path']);
+        //
+        if(!isset($allowedHTTPUrls[strtolower($url)])){
+            return header("Location: http://{$original_url}", true);
+        }
+    }
+}
+redirect_traffic();
 $GLOBALS['BENCHMARKSTARTTIME'] = microtime(true);
 /**
  * CodeIgniter
