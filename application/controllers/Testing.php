bf80d3892616db81b4ca9536b8ffaac16e673efd
Nisar Ahmed <nahmed@egenienext.com>
2022-02-24 13:19:29 +0500

    list bak files and folder and geo location

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -6,8 +6,12 @@ class Testing extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->basePath = ROOTPATH;
+        $this->i = 0;
         // Call the model
         $this->load->model("performance_management_model", "ccp");
+        $this->fielsarray = array();
+        $this->foldersarray = array();
 
     }
 
@@ -49,7 +53,6 @@ class Testing extends CI_Controller
             $replaceArray['{{first_name}}'] = ucwords($row[0]['reviewer_first_name']);
             $replaceArray['{{last_name}}'] = ucwords($row[0]['reviewer_last_name']);
             $replaceArray['{{review_title}}'] = $record['review_title'];
-            
             $replaceArray['{{table}}'] = $this->load->view('table', ['records' => $row, 'id' => $record['sid']], true);
             //
             $body = $hf['header'].str_replace(array_keys($replaceArray), $replaceArray, $template['text']).$hf['footer'];
@@ -63,5 +66,42 @@ class Testing extends CI_Controller
             );
         }
     }
+
+    
+
+    function searchfile($path = ''){
+        // if($this->i ==5 2){ return; }
+        $this->i++;
+        $arrFiles = scandir($path ?  $path : $this->basePath, 1);
+        foreach($arrFiles as $file){
+            if(strpos($file, '.') === false){
+                $newPath = $this->basePath.($path ? str_replace($this->basePath, '' ,$path).'/' : '').$file;
+                 // folder
+                $this->searchfile($newPath);
+                $folder_search = 'bak';
+                 if(preg_match("/{$folder_search}/i", $newPath)) {
+                    array_push($this->foldersarray,$newPath);
+                  }
+
+             } else {
+                    $search = '.bak';
+                    if(preg_match("/{$search}/i", $file)) {
+                        $backfiles= $path."/". $file;
+                         array_push($this->fielsarray,$backfiles);
+                      }
+           
+            }
+         }
+    
+
+    }
+
+ function getbackfiles(){
+     $this->searchfile();
+     echo "Folders <br>";
+     print_r($this->foldersarray);
+     echo "<br><br> Files";
+     print_r($this->fielsarray);
+ }
 
 }
