b273ed0b82150dd71d5de336c64d81b2ad7adbad
aleem <ashaukat@egenie.com>
2022-03-17 18:06:44 +0500

    fix die()

--- application/controllers/Employee_management.php
+++ application/controllers/Employee_management.php
@@ -1370,10 +1370,7 @@ class Employee_management extends Public_Controller {
                         $file = explode(".", $_FILES['pictures']['name']);
                         $file_name = str_replace(" ", "-", $file[0]);
                         $pictures = $file_name . '-' . generateRandomString(6) . '.' . $file[1];
-
-                        //generate_image_compressed($_FILES['pictures']['tmp_name'], 'images/' . $pictures);
-                        // $aws->putToBucket($pictures, 'images/' . $pictures, AWS_S3_BUCKET_NAME);
-                        //
+                        // generate_image_compressed($_FILES['pictures']['tmp_name'], 'images/' . $pictures);
                         $aws = new AwsSdk();
                         $aws->putToBucket($pictures, $_FILES['pictures']['tmp_name'], AWS_S3_BUCKET_NAME);
                     }
