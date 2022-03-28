1bafc80ce3c03c5a7e4a92c0ff3e4c88e0235aa9
Umar <umarilyas595@gmail.com>
2022-03-27 23:19:42 -0700

    Fixed session issue on DB

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -614,7 +614,7 @@ abstract class CI_DB_driver {
 		//
 		$logged_in_user_id = 0;
 		//
-		if($_this->session){
+		if(!is_array($_this->session)){
 			if($_this->session->userdata('logged_in')){
 				$logged_in_user_id = $_this->session->userdata('logged_in')['employer_detail']['sid'];
 			} else if($_this->session->userdata('user_id')){
