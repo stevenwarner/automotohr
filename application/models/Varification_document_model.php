d49f8615de8a60bf738c8392cd30f493a571f7f0
aleem <ashaukat@egenie.com>
2022-06-17 15:49:11 +0500

    14

--- application/models/Varification_document_model.php
+++ application/models/Varification_document_model.php
@@ -265,13 +265,13 @@ class Varification_document_model extends CI_Model {
     //
     public function getMyApprovalDocuments ($employee_sid) {
         //
-        $this->db->select('portal_document_assign_flow_employees.sid'); 
+        $this->db->select('portal_document_assign_flow_employees.sid as flow_employee_sid'); 
         $this->db->select('portal_document_assign_flow_employees.portal_document_assign_sid'); 
         $this->db->select('portal_document_assign_flow_employees.assign_on');
 
         $this->db->select('documents_assigned.has_approval_flow');
         $this->db->select('documents_assigned.approval_process');
-        $this->db->select('documents_assigned.sid');
+        $this->db->select('documents_assigned.sid as document_sid_ins');
         $this->db->where('portal_document_assign_flow_employees.assigner_sid', $employee_sid);
         $this->db->where('portal_document_assign_flow_employees.status', 1);
         $this->db->where('portal_document_assign_flow_employees.assigner_turn', 1);
