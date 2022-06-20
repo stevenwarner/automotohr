cabd7a09844f92ff80cf58ba363389302de9fdd3
aleem <ashaukat@egenie.com>
2022-06-20 17:03:56 +0500

    add note for username

--- application/views/manage_employer/employee_login_credentials_view.php
+++ application/views/manage_employer/employee_login_credentials_view.php
@@ -26,9 +26,10 @@
                                                 <?php echo form_input('email', set_value('email', $employer['email']), 'class="invoice-fields"'); ?>
                                                 <?php echo form_error('email'); ?>
                                             </li>
-                                            <li class="form-col-100">
+                                            <li class="form-col-100" style="margin-bottom: 36px;">
                                                 <?php echo form_label('USERNAME <span class="hr-required">*</span>', 'username'); ?> Username should consist of a minimum of 5 characters.
                                                 <?php echo form_input('username', set_value('username', $employer['username']), 'class="invoice-fields"'); ?>
+                                                <p><strong>Note: </strong>Please create a Username for your Employee / Team member using their first and last name all one word all lower case. Example: johnsmith</p>
                                                 <?php echo form_error('username'); ?>
                                             </li>
                                             <li class="form-col-100">
