89fdbbcf7982446c74ff3f63e5a7ce16547df8e3
Nisar Ahmed <nahmed@egenienext.com>
2022-06-30 19:20:54 +0500

    AHR-0023

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -9,4 +9,28 @@ class Testing extends CI_Controller
         // Call the model
         $this->load->model("test_model", "tm");
     }
+
+
+//
+    public function sync_header_video_overlay()
+    {
+        $this->db->select('company_id, meta_key, meta_value');
+        $this->db->where('meta_key', 'site_settings');
+        $result = $this->db->get('portal_themes_meta_data')->result_array();
+
+        if (!empty($result)) {
+            foreach ($result as $row) {
+                $meta_values = unserialize($row['meta_value']);
+                if (isset($meta_values['enable_header_overlay'])) {
+                    $data =  array(
+                        'header_video_overlay' => $meta_values['enable_header_overlay']
+                    );
+
+                    $this->db->where('user_sid', $row['company_id']);
+                    $this->db->update('portal_employer', $data);
+                }
+            }
+        }
+    
+    }
 }
