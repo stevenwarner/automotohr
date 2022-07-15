f20cbe0d37ce534e0c7e95a313dc3f7b1cf5decf
aleem <ashaukat@egenie.com>
2022-07-15 10:17:11 +0500

    remove text from  view

--- manage_portal/application/views/theme-4/_parts/page_banner.php
+++ manage_portal/application/views/theme-4/_parts/page_banner.php
@@ -18,8 +18,8 @@
         </div>
         <div class="slider-text">
             <div class="slide-inner">
-                <h2 class="slider-title"  <?=isset($section_01_meta['do_capitalize']) && $section_01_meta['do_capitalize'] == 0 ? 'style="text-transform: none;"' : ''; ?>><?php echo (isset($section_01_meta['image']) && trim($section_01_meta['title']) != '' ? $section_01_meta['title'] : 'Are You Looking for an Amazing Career Opportunity?' ); ?></h2>
-                <p class="slide-description"  <?=isset($section_01_meta['do_capitalize']) && $section_01_meta['do_capitalize'] == 0 ? 'style="text-transform: none;"' : ''; ?>><?php echo (isset($section_01_meta['image']) && trim($section_01_meta['tag_line']) != '' ? $section_01_meta['tag_line'] : 'There is No Better Place!' ); ?></p>
+                <h2 class="slider-title"  <?=isset($section_01_meta['do_capitalize']) && $section_01_meta['do_capitalize'] == 0 ? 'style="text-transform: none;"' : ''; ?>><?php echo (isset($section_01_meta['image']) && trim($section_01_meta['title']) != '' ? $section_01_meta['title'] : '' ); ?></h2>
+                <p class="slide-description"  <?=isset($section_01_meta['do_capitalize']) && $section_01_meta['do_capitalize'] == 0 ? 'style="text-transform: none;"' : ''; ?>><?php echo (isset($section_01_meta['image']) && trim($section_01_meta['tag_line']) != '' ? $section_01_meta['tag_line'] : '' ); ?></p>
             </div>
         </div>
     </section>
