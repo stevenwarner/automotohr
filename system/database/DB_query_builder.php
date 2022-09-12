c37afd1c0b175acb7e182e37051c7c01862e7233
aleem <ashaukat@egenie.com>
2022-09-12 11:23:10 +0500

    get array

--- system/database/DB_query_builder.php
+++ system/database/DB_query_builder.php
@@ -1384,7 +1384,7 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 		$result = $this->query($this->_compile_select());
 		$this->_reset_select();
 		if ($result) {
-			return array();
+			return [];
 		}
 		return $result;
 	}
