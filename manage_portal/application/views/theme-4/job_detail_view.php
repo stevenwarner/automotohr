444e45b187d4baaed02ad3c33727d3c1a0a707ae
aleem <ashaukat@egenie.com>
2022-09-17 16:33:23 +0500

    fix url issue

--- manage_portal/application/views/theme-4/job_detail_view.php
+++ manage_portal/application/views/theme-4/job_detail_view.php
@@ -50,7 +50,7 @@
                     <div class="bottom-btn-row">
                         <ul>
                             <li><a href="javascript:;" class="site-btn bg-color apply-now-large" data-toggle="modal" data-target="#myModal">apply now</a></li>
-                            <li><a href="<?php echo $more_career_oppurtunatity; ?>" class="site-btn bg-color-v3">More Career Opportunities With This Company</a></li>
+                            <li><a href="<?php echo strtolower(str_replace(" ","_",$more_career_oppurtunatity)); ?>" class="site-btn bg-color-v3">More Career Opportunities With This Company</a></li>
                         </ul>
                     </div>
                 </div>
