4b684bf2a84d459be6e4df9984b84da90a1b5efe
Mustafa Mehmood <mustafa.mehmood.odesk@gmail.com>
2022-11-09 20:40:28 -0800

    Questions structure

---
+++ application/views/es/partials/step_details.php
@@ -0,0 +1,55 @@
+<!--  -->
+<div class="panel panel-default _csMt20 _csPR _csR5">
+    <div class="panel-heading">
+        <h3 class="_csF16">Survey Details</h3>
+    </div>
+    <div class="panel-body">
+        <form action="javascript:void(0)">
+            <!--  -->
+            <div class="row">
+                <div class="col-md-3 col-sm-12">
+                    <label>Title <span class="text-danger">*</span></label>
+                    <p>What the survey will be called. e.g. "Pre Open Enrollment Survey"</p>
+                </div>
+                <div class="col-md-9 col-sm-12">
+                    <input type="text" class="form-control" required />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-12 _csMt10">
+                    <label>Description </label>
+                    <p>What this survey is about.</p>
+                </div>
+                <div class="col-md-9 col-sm-12 _csMt10">
+                    <textarea class="form-control" row="30"></textarea>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-3 col-sm-12 _csMt10">
+                    <label>Survey Period </label>
+                    <p>When the survey will start and end.</p>
+                </div>
+                <div class="col-md-3 col-sm-12 _csMt10">
+                    <input type="text" class="form-control" placeholder="MM/DD/YYY">
+                </div>
+                <div class="col-md-1 col-sm-12 text-center _csVm _csMt10">
+                    <i class="fa fa-minus" aria-hidden="true"></i>
+                </div>
+                <div class="col-md-3 col-sm-12 _csMt10">
+                    <input type="text" class="form-control" placeholder="MM/DD/YYY">
+                </div>
+            </div>
+            <!-- Footer -->
+            <div class="row">
+                <hr />
+                <div class="col-sm-12 text-right">
+                    <button class="btn _csB4 _csF2 _csR5 _csF14">
+                        <i class="fa fa-long-arrow-right _csF14" aria-hidden="true"></i>
+                        Next
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
---
+++ application/views/es/partials/step_questions.php
@@ -0,0 +1,58 @@
+<!--  -->
+<div class="panel panel-default _csMt20 _csPR _csR5">
+    <div class="panel-heading">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <h3 class="_csF16">Survey Questions <span>(0)</span></h3>
+            </div>
+            <div class="col-md-6 col-sm-12 text-right">
+                <button class="btn _csB4 _csF2 _csR5 _csMt10 _csF16">
+                    <i class="fa fa-plus-circle _csF16" aria-hidden="true"></i>&nbsp;Add Question
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="panel-body">
+        <!-- q1 -->
+        <div class="jsBox">
+            <div class="row">
+                <div class="col-md-6">
+                    <label>Question 1</label>
+                </div>
+                <div class="col-md-6 text-right">
+                    <button class="btn _csR5"> <i class="fa fa-long-arrow-up" aria-hidden="true"></i></button>
+                    <button class="btn _csR5"> <i class="fa fa-long-arrow-down" aria-hidden="true"></i></button>
+                    <button class="btn btn-warning _csR5"> <i class="fa fa-pencil" aria-hidden="true"></i></button>
+                    <button class="btn btn-danger _csR5"> <i class="fa fa-trash" aria-hidden="true"></i></button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="_csF14">Overall, I am satisfied with the benefits package my organization offers.</h4>
+                </div>
+            </div>
+            <div class="row _csMt10">
+                <div class="col-md-2 text-center _csP10">
+                    <p>1</p>
+                    <p>Strongly Disagree</p>
+                </div>
+                <div class="col-md-2 text-center _csP10">
+                    <p>2</p>
+                    <p>Strongly Disagree</p>
+                </div>
+                <div class="col-md-2 text-center _csP10">
+                    <p>3</p>
+                    <p>Strongly Disagree</p>
+                </div>
+                <div class="col-md-2 text-center _csP10">
+                    <p>4</p>
+                    <p>Strongly Disagree</p>
+                </div>
+                <div class="col-md-2 text-center _csP10">
+                    <p>5</p>
+                    <p>Strongly Disagree</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
