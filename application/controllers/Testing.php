caa488fe6990a3f54a20f3e2d3d551c2b1438802
aleem <ashaukat@egenie.com>
2022-11-15 17:29:55 +0500

    testing

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -53,4 +53,27 @@ class Testing extends CI_Controller
         fclose($file);
         die("ppak");
     }
+
+    public function myfunction () {
+        //
+        
+        //
+        $curl = curl_init();
+        //
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "http://localhost:3000/employee_survey/templates",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+        //
+        $response = json_decode(curl_exec($curl), true);
+        //
+        curl_close($curl);
+        _e($response,true,true);
+    }
 }
