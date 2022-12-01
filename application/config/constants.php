adfc57a09d86e3d4cf79f42ff40f0b450cad93fa
Nisar Ahmed <nahmed@egenienext.com>
2022-12-01 17:06:30 +0500

    dtpickerdatechange

--- application/config/constants.php
+++ application/config/constants.php
@@ -530,9 +530,9 @@ define('FMLA_RIGHTS', 'To be eligible an employee must have worked for an employ
 define('TIMEOFFYEARLYCOMPANY', '58,5635');
 //
 define('ASSIGNEDOCIMPL', TRUE);
-define('DOB_LIMIT', '-100:+1');
-define('JOINING_DATE_LIMIT', '1960:+5');
-define('STARTING_DATE_LIMIT', '1960:+20');
+define('DOB_LIMIT', '1910:+1');
+define('JOINING_DATE_LIMIT', '1910:+5');
+define('STARTING_DATE_LIMIT', '1910:+20');
 define('DATE', 'M d Y, D');
 define('DB_DATE', 'Y-m-d');
 define('DATE_WITH_TIME', 'M d, Y, D H:m:s');
