1bb2af593f652823866e5f93d0b669af4083e83d
aleem <ashaukat@egenie.com>
2022-12-14 12:50:59 +0500

    add logo and company name

--- application/views/manage_employer/application_tracking_system/index.php
+++ application/views/manage_employer/application_tracking_system/index.php
@@ -8,6 +8,12 @@
                 <div class="dashboard-conetnt-wrp">
                     <div class="page-header-area">
                         <span class="page-heading down-arrow">
+                            <?php if(!empty($session['company_detail']['Logo'])) { ?>
+                                <img src="<?php echo 'https://automotohrattachments.s3.amazonaws.com/'.$session['company_detail']['Logo'] ?>" style="width: 75px; height: 75px;" class="img-rounded"><br>
+                            <?php } ?>
+                            <?php if(!empty($session['company_detail']['CompanyName'])) { ?>
+                                <?php echo $session['company_detail']['CompanyName']; ?><br>
+                            <?php } ?>
                             <?php echo $title; ?>
                         </span>
                     </div>
