8be773b77879c66800a6d73ddf96fa3fa88d5025
Mubashir Ahmed <mubashar@automotohr.com>
2023-01-13 11:58:06 -0800

    Replaced username with firstname

--- executive_admin/application/models/Users_model.php
+++ executive_admin/application/models/Users_model.php
@@ -519,7 +519,7 @@ class Users_model extends CI_Model {
         ->or_like('portal_job_applications.email', $query)
         ->group_end()
         ->limit($offset, $inset)
-        ->order_by('user_name', 'ASC')
+        ->order_by('first_name', 'ASC')
         ->where_in('portal_job_applications.employer_sid', $executiveCompanyIds, false)
         ->where('portal_job_applications.hired_sid IS NULL', NULL)
         ->get();
