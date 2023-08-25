c3b59ffa4e532303f734f15185cfd817997e6571
Mubashar Ahmed <mubashar@automotohr.com>
2023-08-25 12:45:18 +0500

    Added new bundle logic

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -608,10 +608,12 @@ if (!function_exists('getStaticFileVersion')) {
      * set and get the version of the minified file
      *
      * @param string $file The URI of the asset
+     * @param string   $newFlow
      * @returns string
      */
     function getStaticFileVersion(
-        string $file
+        string $file,
+        string $newFlow = ''
     ) {
         // set files
         $files = [];
@@ -629,7 +631,7 @@ if (!function_exists('getStaticFileVersion')) {
         // set the main CSS file
         $files['2022/css/main'] = ['css' => '2.1.1'];
         // set the course files
-        $files['assets/app_helper'] = ['js' => '3.0.0'];
+        $files['js/app_helper'] = ['js' => '3.0.0'];
         $files['v1/common'] = ['js' => '3.0.0'];
         $files['v1/lms/add_question'] = ['js' => '3.0.0'];
         $files['v1/lms/edit_question'] = ['js' => '3.0.0'];
@@ -639,7 +641,7 @@ if (!function_exists('getStaticFileVersion')) {
         $files['v1/lms/assign_company_courses'] = ['js' => '3.0.0'];
         $files['v1/lms/preview_assign'] = ['js' => '3.0.0'];
         // check and return data
-        return $files[$file] ?? [];
+        return $newFlow ? ($files[$file][$newFlow] ?? '1.0.0'): ($files[$file] ?? []);
     }
 }
 
