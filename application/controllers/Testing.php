571f5d80e91360ea78375924540c95214a232f2a
Nisar Ahmed <nisarahmed@egenienext.com>
2023-08-25 18:10:24 +0500

    feature/complynet_v12

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -65,16 +65,21 @@ class Testing extends CI_Controller
 
         //
         $complyNetEmployeeData = $this->db
-            ->select('complynet_employees.sid,complynet_employees.alt_id,complynet_employees.complynet_employee_sid,complynet_employees.complynet_company_sid,complynet_employees.complynet_location_sid,complynet_employees.complynet_department_sid,complynet_employees.complynet_job_role_sid,complynet_employees.employee_sid,complynet_employees.email,complynet_employees.complynet_json,users.first_name,users.last_name,users.active,users.terminated_status')
+            ->select('complynet_employees.sid,complynet_employees.company_sid,complynet_employees.alt_id,complynet_employees.complynet_employee_sid,complynet_employees.complynet_company_sid,complynet_employees.complynet_location_sid,complynet_employees.complynet_department_sid,complynet_employees.complynet_job_role_sid,complynet_employees.employee_sid,complynet_employees.email,complynet_employees.complynet_json,users.first_name,users.last_name,users.active,users.terminated_status,users.sid')
             ->where('complynet_employees.company_sid', $companyId)
             ->join('users', 'users.sid = complynet_employees.employee_sid')
             ->get('complynet_employees')
             ->result_array();
 
+        //   _e($complynetUserData,true,true);
         // _e($complyNetEmployeeData, true, true);
         //
         $userData = [];
         $missingData = [];
+
+        $totalAhrActiveStatus = 0;
+        $totalAhrInactiveStatus = 0;
+
         foreach ($complyNetEmployeeData as $row) {
             if ($complynetUserData[$row['complynet_employee_sid']]) {
                 $missingfield = array();
@@ -115,6 +120,15 @@ class Testing extends CI_Controller
                     $status = 1;
                 }
 
+                //
+                if ($status == 1 && $complynetUserDataRow['Status'] != 1) {
+                    $totalAhrActiveStatus++;
+                }
+
+                if ($status == 0 && $complynetUserDataRow['Status'] == 1) {
+                    $totalAhrInactiveStatus++;
+                }
+
 
                 if ($complynetUserDataRow['Status'] != $status) {
                     $missingfield[] = array('complynet_status' => $complynetUserDataRow['Status'], 'ahr_status' => $row['active'], 'ahr_terminated_status' => $row['terminated_status']);
@@ -123,6 +137,12 @@ class Testing extends CI_Controller
                 // $complynet_json = json_decode($row['complynet_json'], true);
 
                 $missingData['missingdata'] = $missingfield;
+                $datadetail['ahr_userId'] = $row['sid'];
+                $datadetail['complynet_userId'] = $complynetUserDataRow['UserId'];
+
+
+
+
                 $datadetail['complynet_name'] = $complynetUserDataRow['FirstName'] . ' ' . $complynetUserDataRow['LastName'];
                 $datadetail['ahr_name'] = $row['first_name'] . ' ' . $row['last_name'];
                 $datadetail['ahr_company_sid'] = $row['company_sid'];
@@ -131,7 +151,60 @@ class Testing extends CI_Controller
                 $userData[] = $datadetail;
             }
         }
+
+        //  $userData['Total_Status_Active_On_AHR_and_Inactive_OnComplyNet'] = $totalAhrActiveStatus;
+        // $userData['Total_Status_Inactive_On_AHR_and_Active_OnComplyNet'] = $totalAhrInactiveStatus;
+
         // return $userData;
+
+
+
+        // CSV Export
+
+        $fileName = 'ComplyNetStatusMissmatch_' . date('d_m_Y_H_i_s', strtotime('now'));
+        //
+        header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename=' . ($fileName) . '.csv');
+        $handler = fopen('php://output', 'w');
+        fputcsv($handler, ['ahr_userId', 'complynet_userId', 'ahr_name', 'complynet_name', 'ahr_company_sid', 'ahr_status', 'complynet_status']);
+
+
+        foreach ($userData as $dataRow) {
+            //_e($dataRow, true);
+            // missingdata
+            $ahrStatus = '';
+            $complyNetStatus = '';
+            if ($dataRow['missingdata']) {
+
+                foreach ($dataRow['missingdata'] as $missingRow) {
+
+                    if ($missingRow['complynet_status']) {
+                        $complyNetStatus = $missingRow['complynet_status'] == 1 ? 'Active' : 'InActive';
+                        $ahrStatus = $missingRow['ahr_status'] == 1 ? 'Active' : 'InActive';
+                    }
+                }
+            }
+
+
+            if($ahrStatus !='' && $complyNetStatus!=''){
+            fputcsv($handler, [
+                $dataRow['ahr_userId'],
+                $dataRow['complynet_userId'],
+                $dataRow['ahr_name'],
+                $dataRow['complynet_name'],
+                $dataRow['ahr_company_sid'],
+                $ahrStatus,
+                $complyNetStatus,
+            ]);
+        }
+
+
+        }
+
+        fclose($handler);
+        exit(0);
+
+
         _e($userData, true);
     }
 }
