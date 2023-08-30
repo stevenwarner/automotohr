2c65409c1e42bc6fd73b1c035a40293e4f891ca9
Nisar Ahmed <nisarahmed@egenienext.com>
2023-08-30 17:39:45 +0500

    AHR-3133

--- application/models/Expirations_autoresponder_model.php
+++ application/models/Expirations_autoresponder_model.php
@@ -26,7 +26,7 @@ class Expirations_autoresponder_model extends CI_Model
 
     function GetAllActiveCompanies(){
         $this->db->select('*');
-        $this->db->where('parent_sid', '');
+        $this->db->where('parent_sid', '0');
         $this->db->where('active', 1);
         $this->db->where('career_page_type', 'standard_career_site');
         $result = $this->db->get('users')->result_array();
