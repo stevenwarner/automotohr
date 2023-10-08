3c6d9ed8e72649e0df83017b5f96d03baf56c472
Aleem Shaukat <ashaukat@egenienext.com>
2023-10-08 10:08:42 +0500

    90% dome with resources

---
+++ public/v1/js/app/resources.js
@@ -0,0 +1,14 @@
+$(function resources() {
+	// set the xhr
+	let XHR = null;
+
+	/**
+	 * Preview course
+	 */
+	$(document).on("click", ".jsLoadMoreBlog", function (event) {
+		// call the function
+		console.log("love you")
+	});
+
+    console.log("herei amS")
+});
