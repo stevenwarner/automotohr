a1d31366dbdce5d2850e28dd88747d39ac3a583e
Mubashar Ahmed <mubashar@automotohr.com>
2023-10-13 11:16:45 +0500

    fixed hours issue

--- assets/v1/payroll/js/off_cycle/hours_and_earnings.js
+++ assets/v1/payroll/js/off_cycle/hours_and_earnings.js
@@ -385,7 +385,7 @@ $(function regularPayrollsHoursAndEarnings() {
 					rows += '	<td colspan="6" class="vam text-right">';
 					rows +=
 						'		<a href="' +
-						baseUrl("payrolls/"+payrollReason) +
+						baseUrl("payrolls/" + payrollReason) +
 						'" class="btn csW csBG4 csF16">';
 					rows +=
 						'			<i class="fa fa-times-circle csF16" aria-hidden="true"></i>';
@@ -425,18 +425,21 @@ $(function regularPayrollsHoursAndEarnings() {
 			//
 			const payrollEmployee = payroll["employees"][employeeId];
 			if (payrollEmployee && !payrollEmployee.excluded) {
-				// for regular hours
-				$(this)
-					.find(".jsRegularHoursText")
-					.text(
-						`${payrollEmployee.hourly_compensations.regular_hours.hours} hrs`
-					);
-				//
-				$(this)
-					.find(".jsRegularHoursValue")
-					.val(
-						payrollEmployee.hourly_compensations.regular_hours.hours
-					);
+				if (payrollEmployee.hourly_compensations.regular_hours) {
+					// for regular hours
+					$(this)
+						.find(".jsRegularHoursText")
+						.text(
+							`${payrollEmployee.hourly_compensations.regular_hours.hours} hrs`
+						);
+					//
+					$(this)
+						.find(".jsRegularHoursValue")
+						.val(
+							payrollEmployee.hourly_compensations.regular_hours
+								.hours
+						);
+				}
 
 				if (payrollEmployee.hourly_compensations.overtime) {
 					// for overtime
