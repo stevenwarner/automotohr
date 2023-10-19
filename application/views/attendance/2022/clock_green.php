504ef2eaf3123ad72316392734a5d5cb4727ee0e
Mubashar Ahmed <mubashar@automotohr.com>
2023-10-19 17:17:13 +0500

    Added plus check

--- application/views/attendance/2022/clock_green.php
+++ application/views/attendance/2022/clock_green.php
@@ -1,7 +1,25 @@
+<?php if (checkIfAppIsEnabled('attendance') && isPayrollOrPlus()) { ?>
+    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-6">
+        <div class="dash-box">
+            <div class="dashboard-widget-box">
+                <figure><i class="fa fa-pie-chart" aria-hidden="true"></i></figure>
+                <h2 class="post-title">
+                    <a href="<?= base_url('attendance/overview'); ?>">Attendance Management</a>
+                </h2>
+                <div class="count-box" style="font-size: 12px">
+                    <small style="font-size: 12px"></small>
+                </div>
+                <div class="button-panel">
+                    <a href="<?= base_url('attendance/overview'); ?>" class="site-btn">Manage</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
 <?php if (checkIfAppIsEnabled('attendance')) { ?>
     <div class="col-lg-4 col-md-4 col-xs-12 col-sm-6">
         <div class="dash-box">
-            <a href="<?=base_url('attendance/my');?>">
+            <a href="<?= base_url('attendance/my'); ?>">
                 <div class="dashboard-widget-box">
                     <div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; width: 100%; z-index: 1; background: rgba(255,255,255,.9);" class="jsAttendanceLoader">
                         <i class="fa fa-circle-o-notch fa-spin text-center csF40 csFC3" style="margin-top: 90px;" aria-hidden="true"></i>
