fea6cfda79e0a679bb8e54b38fb38be20adb60a0
Mubashar Ahmed <mubashar@automotohr.com>
2023-10-25 14:25:57 +0500

    Chnaged payrolls to payroll

--- application/views/v1/payroll/historical_info.php
+++ application/views/v1/payroll/historical_info.php
@@ -2,11 +2,11 @@
     <div class="col-sm-12">
         <p class="csF16 text-danger">
             <strong>
-                <em>Please make sure to run historical payrolls first before running regular payrolls.</em>
+                <em>Please make sure to run historical payroll first before running regular payroll.</em>
             </strong>
         </p>
         <p class="csF16">
-            Historical payrolls are used for companies who transfer from one payroll provider to your payroll product after the start of a year. It's important to capture their previous payrolls during Company Onboarding to ensure year to date filings are accurate.
+            Historical payroll are used for companies who transfer from one payroll provider to your payroll product after the start of a year. It's important to capture their previous payroll during Company Onboarding to ensure year to date filings are accurate.
         </p>
         <p class="csF16">
             An employer will need to gather all paystubs for every employee who was paid in the current calendar year. For previous quarters in this calendar year, the employer should gather quarterly payroll summaries for each employee and use these to enter quarter-to-date ("QTD") information to simplify this step (1 payroll per calendar quarter). These summaries should include any benefits, reimbursements, or exemptions for their employees.
