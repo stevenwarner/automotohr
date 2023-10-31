f07dcf02db6571b1cdf9d3e5ed4a9de44d25155a
Mubashar Ahmed <mubashar@automotohr.com>
2023-10-31 18:06:48 +0500

    Changed the schedule popup

--- application/views/v1/app/partials/schedule_demo_form_popup.php
+++ application/views/v1/app/partials/schedule_demo_form_popup.php
@@ -1,13 +1,27 @@
+<style>
+    .modal-content {
+        border-radius: 40px;
+        background-color: #F1F6F9;
+    }
+</style>
 <div class="modal fade" id="jsScheduleDemoModal">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Schedule your free demo</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
             <div class="modal-body">
+                <div class="form_section px-3 d-flex flex-column justify-content-center">
+                    <div class="d-flex flex-column align-items-center mb-5">
+                        <br />
+                        <p class="sora-family autmotoPara automotoGreytext">
+                            <span class="highlighted-light-blue-div">Look</span> in Your Demo
+                        </p>
+                        <h3 class="automotoH3 darkGreyColor line_height ">
+                            Contact Our Talent Partners
+                        </h3>
+                    </div>
+                </div>
                 <?php $this->load->view("v1/app/partials/demo_form", [
-                    "buttonClass"  => "w-100"
+                    "buttonClass"  => "w-100",
+                    "buttonText" => "Schedule My Free Demo"
                 ]); ?>
             </div>
         </div>
