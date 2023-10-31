49efb9c4558de03494a973300abfce5455fc7df9
Mubashar Ahmed <mubashar@automotohr.com>
2023-10-31 17:49:07 +0500

    fixed bs modal close

--- application/views/v1/app/partials/schedule_demo_form_popup.php
+++ application/views/v1/app/partials/schedule_demo_form_popup.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Schedule your free demo</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <?php $this->load->view("v1/app/partials/demo_form", [
