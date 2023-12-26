b34d25d6508b832945da7e474025a5102412c4e8
Mubashar Ahmed <mubashar@automotohr.com>
2023-12-26 15:17:22 +0500

    added payroll dashboard for employee

---
+++ application/models/v1/Users/Main_model.php
@@ -0,0 +1,153 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+/**
+ * Main model
+ * 
+ * @author  AutomotoHR Dev Team
+ * @link    www.automotohr.com
+ * @version 1.0
+ * @package Payroll dashboard
+ */
+class Main_model extends CI_Model
+{
+    /**
+     * main entry point
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * get the employee details
+     *
+     * @param int $employeeId
+     * @param array $columns Optional
+     * @return array
+     */
+    public function getEmployeeDetails(int $employeeId, array $columns = ["*"]): array
+    {
+        return $this->db
+            ->select($columns)
+            ->where("sid", $employeeId)
+            ->get("users")
+            ->row_array();
+    }
+
+    /**
+     * get the company pay schedules
+     *
+     * @param int $companyId
+     * @return array
+     */
+    public function getCompanyPaySchedules(
+        int $companyId
+    ): array {
+        return $this->db
+            ->select("sid, frequency, anchor_pay_date, day_1, day_2, custom_name")
+            ->where([
+                "company_sid" => $companyId,
+                "active" => 1
+            ])
+            ->order_by("sid", "DESC")
+            ->get("companies_pay_schedules")
+            ->result_array();
+    }
+
+    /**
+     * get the user pay schedule
+     *
+     * @param int    $userId
+     * @param string $userType
+     * @param bool   $fullInfo Optional
+     * @return array
+     */
+    public function getUserPayScheduleById(
+        int $userId,
+        string $userType,
+        bool $fullInfo = false
+    ): array {
+        $record = $this->db
+            ->select("pay_schedule_sid")
+            ->where([
+                "user_sid" => $userId,
+                "user_type" => $userType
+            ])
+            ->get("users_pay_schedule")
+            ->row_array();
+        //
+        if (!$record || !$fullInfo) {
+            return $record;
+        }
+        // attach the info
+        // get the pay schedule info
+        $info = $this->db
+            ->select("frequency, anchor_pay_date, day_1, day_2, custom_name")
+            ->where("sid", $record["pay_schedule_sid"])
+            ->get("companies_pay_schedules")
+            ->row_array();
+        //
+        return array_merge($record, $info);
+    }
+
+    /**
+     * process the pay schedule
+     *
+     * @param int    $userId
+     * @param string $userType
+     * @param array  $post
+     * @return array
+     */
+    public function processPaySchedule(
+        int $userId,
+        string $userType,
+        array $post
+    ): array {
+        //
+        $status = 400;
+        $response = ["msg" => "Something went wrong while updating users pay schedule."];
+        // validation
+        // set up the rules
+        $this->form_validation->set_rules("pay_schedule", "Pay schedule name", "trim|xss_clean|required");
+        // run the validation
+        if (!$this->form_validation->run()) {
+            return SendResponse($status, getFormErrors());
+        }
+        // set where array
+        $whereArray = [
+            "user_sid" => $userId,
+            "user_type" => $userType,
+        ];
+        // check if entry already exists
+        if (!$this->db->where($whereArray)->count_all_results("users_pay_schedule")) {
+            // insert
+            $this->db
+                ->insert("users_pay_schedule", [
+                    "user_sid" => $userId,
+                    "user_type" => $userType,
+                    "pay_schedule_sid" => $post["pay_schedule"],
+                    "created_at" => getSystemDate(),
+                    "updated_at" => getSystemDate(),
+                ]);
+            // check and insert log
+            if ($this->db->insert_id()) {
+                //
+                $status = 200;
+                $response = ["msg" => "You have successfully updated the user pay schedule."];
+            }
+        } else {
+            // update
+            $this->db
+                ->where($whereArray)
+                ->update("users_pay_schedule", [
+                    "pay_schedule_sid" => $post["pay_schedule"],
+                    "updated_at" => getSystemDate(),
+                ]);
+            //
+            $status = 200;
+            $response = ["msg" => "You have successfully updated the user pay schedule."];
+        }
+        // update
+        //
+        return SendResponse($status, $status === 400 ? ["errors" => [$response["msg"]]] : $response);
+    }
+}
---
+++ application/views/v1/users/payroll/dashboard.php
@@ -0,0 +1,163 @@
+<div class="main-content">
+    <div class="dashboard-wrp">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-9 col-md-9 col-xs-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                            <?php $this->load->view('templates/_parts/admin_flash_message'); ?>
+
+                            <?php $this->load->view('manage_employer/employee_management/employee_profile_ats_view_top'); ?>
+                            <!-- page header -->
+                            <div class="page-header-area margin-top">
+                                <span class="page-heading down-arrow">
+                                    <?php $this->load->view('manage_employer/company_logo_name'); ?>
+                                </span>
+                            </div>
+                            <!-- main buttons area -->
+                            <div class="row">
+                                <div class="col-sm-12 text-right">
+                                    <a class="btn btn-black" href="<?= $return_title_heading_link; ?>">
+                                        <i class="fa fa-arrow-left"></i>
+                                        <?= $return_title_heading; ?>
+                                    </a>
+                                </div>
+                            </div>
+                            <hr />
+                            <!-- main content area -->
+                            <div class="row">
+                                <!-- pay schedule -->
+                                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="text-medium panel-heading-text">
+                                                <i class="fa fa-calendar text-orange" aria-hidden="true"></i>
+                                                Pay schedule
+                                            </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <p class="text-medium">
+                                                <span class="text-small">Name</span>
+                                                <br />
+                                                <strong><?= GetVal($paySchedule["custom_name"]); ?></strong>
+                                            </p>
+                                            <p class="text-medium">
+                                                <span class="text-small">Pay frequency</span>
+                                                <br />
+                                                <strong><?= GetVal($paySchedule["frequency"]); ?></strong>
+                                            </p>
+                                            <?php if ($paySchedule["day_1"] && $paySchedule["day_2"]) { ?>
+                                                <p class="text-medium">
+                                                    <span class="text-small">First day of payment</span>
+                                                    <br />
+                                                    <strong>"<?= GetVal($paySchedule["day_1"]); ?>" of the month</strong>
+                                                </p>
+                                                <p class="text-medium">
+                                                    <span class="text-small">Second day of payment</span>
+                                                    <br />
+                                                    <strong>"<?= GetVal($paySchedule["day_2"]); ?>" of the month</strong>
+                                                </p>
+                                            <?php } ?>
+                                            <p class="text-medium">
+                                                <span class="text-small">Pay period start date</span>
+                                                <br />
+                                                <strong><?= $paySchedule["anchor_pay_date"] ? formatDateToDB($paySchedule["anchor_pay_date"], DB_DATE, DATE) : "Not specified"; ?></strong>
+                                            </p>
+                                        </div>
+                                        <div class="panel-footer text-center">
+                                            <button class="btn btn-yellow jsEditPaySchedule">
+                                                <i class="fa fa-edit" aria-hidden="true"></i>
+                                                Edit Pay schedule
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- wage -->
+                                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="text-medium panel-heading-text">
+                                                <i class="fa fa-money text-orange" aria-hidden="true"></i>
+                                                Job & wage
+                                            </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <p class="text-medium">
+                                                <span class="text-small">Position</span>
+                                                <br />
+                                                <strong>Software Engineer II</strong>
+                                            </p>
+                                            <p class="text-medium">
+                                                <span class="text-small">Employment type</span>
+                                                <br />
+                                                <strong>Full time</strong>
+                                            </p>
+                                            <p class="text-medium">
+                                                <span class="text-small">Hire date</span>
+                                                <br />
+                                                <strong>2023-04-55 time</strong>
+                                            </p>
+                                            <p class="text-medium">
+                                                <span class="text-small">Wage</span>
+                                                <br />
+                                                <strong>$20.00 /hour</strong>
+                                            </p>
+                                        </div>
+                                        <div class="panel-footer text-center">
+                                            <button class="btn btn-yellow jsEditJobWage">
+                                                <i class="fa fa-edit" aria-hidden="true"></i>
+                                                Edit wage
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- rules -->
+                                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="text-medium panel-heading-text">
+                                                <i class="fa fa-money text-orange" aria-hidden="true"></i>
+                                                Overtime rules
+                                            </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <p class="text-medium">
+                                                <strong>40 hours week</strong>
+                                            </p>
+                                            <p class="text-medium">
+                                                <strong>8 hours day, 40 hours week</strong>
+                                            </p>
+                                        </div>
+                                        <div class="panel-footer text-center">
+                                            <button class="btn btn-yellow">
+                                                <i class="fa fa-edit" aria-hidden="true"></i>
+                                                Edit Overtime rules
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- time & attendance -->
+                            <div class="row">
+                                <div class="col-sm-4">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php $this->load->view($left_navigation); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    const profileUserInfo = {
+        userId: <?= $userId; ?>,
+        userType: "<?= $userType; ?>",
+        nameWithRole: "<?= remakeEmployeeName($employer); ?>"
+    };
+</script>
