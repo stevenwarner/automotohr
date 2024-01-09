3180c328778bcad6b05f81b1cbb1ed6d7953f984
Mubashar Ahmed <mubashar@automotohr.com>
2024-01-09 22:39:06 +0500

    fixed the null issue

--- application/controllers/Hr_documents_management.php
+++ application/controllers/Hr_documents_management.php
@@ -16930,7 +16930,7 @@ class Hr_documents_management extends Public_Controller
         $updateArray["emp_name"] = $w4Form["emp_name"] ?? $data["CompanyName"];
         $updateArray["emp_address"] = $w4Form["emp_address"] ?? $data["companyAddress"];
         if (!$w4Form["first_date_of_employment"] && $data["first_date_of_employment"]) {
-            $updateArray["first_date_of_employment"] = $data["first_date_of_employment"] ?? null;
+            $updateArray["first_date_of_employment"] = formatDateForDB($data["first_date_of_employment"], "m-d-Y", DB_DATE);
         }
         $updateArray["emp_identification_number"] = $w4Form["emp_identification_number"] ?? $data["ssn"];
 
