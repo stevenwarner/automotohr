b4df6abc4bfa1159966e3bfb61f7ecfedae6c04e
Aleem Shaukat <ashaukat@egenienext.com>
2024-01-23 09:34:22 +0500

    fix employee job sync

--- application/models/v1/Employee_payroll_model.php
+++ application/models/v1/Employee_payroll_model.php
@@ -695,6 +695,18 @@ class Employee_payroll_model extends Payroll_base_model
                 $ins['adjust_for_minimum_wage'] = $compensation['adjust_for_minimum_wage'];
                 //
                 if ($this->db->where(['gusto_uuid' => $compensation['uuid']])->count_all_results('gusto_employees_jobs_compensations')) {
+                    $previousStatus = $this->db
+                        ->select('flsa_status')
+                        ->where([
+                            'gusto_uuid' => $compensation['uuid']
+                        ])
+                        ->get('gusto_employees_jobs_compensations')
+                        ->row_array()['flsa_status'];
+                    //
+                    if ($previousStatus == "Salaried Commission" && $compensation['flsa_status'] == "Salaried Nonexempt") {
+                        unset($ins['flsa_status']);
+                    }
+                    //
                     $ins['updated_at'] = getSystemDate();
                     $this->db
                         ->where(['gusto_uuid' => $compensation['uuid']])
