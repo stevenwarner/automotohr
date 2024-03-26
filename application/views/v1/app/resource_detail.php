d1e900fec41861e54f90bb085015c6eeb155282d
Nisar Ahmed <nisarahmed@egenienext.com>
2024-03-26 12:15:07 +0500

    AHR-3294

--- application/views/v1/app/resource_detail.php
+++ application/views/v1/app/resource_detail.php
@@ -41,41 +41,38 @@
 
         <div class="row">
             <div class="col-sm-12">
-                <div class="blog-main-image">
-                    <?= makeResourceView($blog["resources"]); ?>
+                <div class="blog-detail-div111">
+                    <div class="para-text">
+                        <div style="padding-bottom: 20px;">
+                            <?= makeResourceView($blog["resources"]); ?>
+                        </div>
 
-                </div>
-            </div>
-        </div>
-
-        <?php if ($downloadLink) { ?>
-            <div class="row">
-                <div class="col-xs-1s2">
-                    <div class="w-80 ">
-                        <div class="row resources-subscribe-row-download ">
-                            <div class="col-xs-12 col-xl-7 column-center">
-                                <div class="file-btn-div subscribe-input-div">
-                                    <input id="" class="upload-file-input subscribe-input" placeholder=" Download eBook " readonly />
-                                    <a class="custom-file-download" target="_blank" href="<?= AWS_S3_BUCKET_URL . $blog['resources']; ?>">
-                                        Download
-                                    </a>
+                        <?php if ($downloadLink) { ?>
+                            <div class="row">
+                                <div class="col-xs-1s2">
+                                    <div class="w-80 ">
+                                        <div class="row resources-subscribe-row-download ">
+                                            <div class="col-xs-12 col-xl-7 column-center">
+                                                <div class="file-btn-div subscribe-input-div">
+                                                    <input id="" class="upload-file-input subscribe-input" placeholder=" Download eBook " readonly />
+                                                    <a class="custom-file-download" target="_blank" href="<?= AWS_S3_BUCKET_URL . $blog['resources']; ?>">
+                                                        Download
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
+                        <?php } ?>
 
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="blog-detail-div ">
-                    <div class="para-text">
                         <?php echo $blog['description']; ?>
+
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 
 </main>
