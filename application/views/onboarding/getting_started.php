082125ebf29410115f52d7c01589c01991d0308c
Nisar Ahmed <nisarahmed@egenienext.com>
2024-11-12 17:51:07 +0500

    add compliance report box

--- application/views/onboarding/getting_started.php
+++ application/views/onboarding/getting_started.php
@@ -784,7 +784,7 @@ $document_d_base = base_url('hr_documents_management/sign_hr_document/d');
                             <div class="col-lg-4 col-md-4 col-xs-12 col-sm-6 ">
                                 <div class="widget-box">
                                     <span id="main">
-                                        <div class="link-box bg-complynet full-width" style="background-color: #8294cc !important">
+                                        <div class="link-box bg-complynet full-width" style="background-color: #8294cc !important;">
                                             <h2>Compliance <br /> Report</h2>
                                             <ul class="cs-jam-ul" style="position: relative; z-index:1;">
                                                 <?php if (!empty($complynet_dashboard_link) && $complynet_dashboard_link != NULL && $access_level != 'Employee') { ?>
@@ -798,7 +798,7 @@ $document_d_base = base_url('hr_documents_management/sign_hr_document/d');
                                                         style="color: #ffffff; padding: 3px;">Login</button></li>
                                             </ul>
                                             <div class="status-panel">
-                                                <h3>Complynet</h3>
+                                                <h3>Report</h3>
                                                 <span>Show</span>
                                             </div>
                                         </div>
