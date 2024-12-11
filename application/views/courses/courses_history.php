2b3f0b049e4e19d9ab91108f1499d351d3a4b935
Mubashir Ahmed <mubashar@automotohr.com>
2024-12-11 13:55:15 +0500

    Changed the text for completion date

--- application/views/courses/courses_history.php
+++ application/views/courses/courses_history.php
@@ -3,12 +3,13 @@
 <div class="main csPageWrap">
     <div class="container-fluid">
         <div style="position: relative">
-            <?php //$this->load->view('loader_new', ['id' => 'jsPageLoader']); ?>
+            <?php //$this->load->view('loader_new', ['id' => 'jsPageLoader']); 
+            ?>
             <?php $this->load->view('courses/partials/my_course_filter'); ?>
             <!--  -->
             <div class="row">
                 <div class="col-lg-3 col-md-3 col-xs-12 col-sm-3">
-                    <a href="<?php echo $employee['access_level'] == 'Employee' ?  base_url('employee_management_system'): base_url('dashboard'); ?>" class="btn btn-info csRadius5"><i class="fa fa-arrow-left"></i> Dashboard</a>
+                    <a href="<?php echo $employee['access_level'] == 'Employee' ?  base_url('employee_management_system') : base_url('dashboard'); ?>" class="btn btn-info csRadius5"><i class="fa fa-arrow-left"></i> Dashboard</a>
                 </div>
             </div>
             <!--  -->
@@ -19,16 +20,16 @@
                             <?php echo $page_title; ?>
                             <div style="float: right;">
                                 <?php if ($page == "my_courses_history") { ?>
-                                    <a href="<?php echo base_url('lms/courses/my_lms_dashboard'); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-laptop"></i> Active Courses</a>
-                                <?php } else if ($page == "employee_courses_history") { ?>   
+                                    <a href="<?php echo base_url('lms/courses/my_lms_dashboard'); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-shield"></i> Active Courses</a>
+                                <?php } else if ($page == "employee_courses_history") { ?>
                                     <?php if ($reviewAs == 'non_plus') { ?>
-                                        <a href="<?php echo base_url('lms/subordinate/dashboard/'.$subordinateId); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-laptop"></i> Active Courses</a>
+                                        <a href="<?php echo base_url('lms/subordinate/dashboard/' . $subordinateId); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-shield"></i> Active Courses</a>
                                     <?php } else if ($reviewAs == 'plus') { ?>
-                                        <a href="<?php echo base_url('lms/employee/courses/dashboard/'.$subordinateId); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-laptop"></i> Active Courses</a>
-                                    <?php } ?>        
+                                        <a href="<?php echo base_url('lms/employee/courses/dashboard/' . $subordinateId); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-shield"></i> Active Courses</a>
+                                    <?php } ?>
                                 <?php } ?>
-                                <?php if ($haveSubordinate == "yes") { ?>
-                                        <a href="<?php echo base_url('lms/courses/report'); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-pie-chart"></i> Team Report</a>
+                                <?php if (isPayrollOrPlus() || $haveSubordinate == "yes") { ?>
+                                    <a href="<?php echo base_url('lms/courses/report'); ?>" class="btn btn-info btn-orange csRadius5 csF16"><i class="fa fa-pie-chart"></i> Team Report</a>
                                 <?php } ?>
                             </div>
                         </h1>
@@ -37,8 +38,8 @@
             </div>
             <!--  -->
             <!--  -->
-            <div class="col-sm-3"> 
-                <?php if ($page == "employee_courses_history") { ?> 
+            <div class="col-sm-3">
+                <?php if ($page == "employee_courses_history") { ?>
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h1 class="panel-heading-text text-medium">
@@ -66,7 +67,7 @@
 
                         </div>
                     </div>
-                <?php } ?>    
+                <?php } ?>
                 <!--  -->
                 <?php if ($history) { ?>
                     <div class="panel panel-default">
@@ -79,7 +80,7 @@
                             <div id="historyContainer"></div>
                         </div>
                     </div>
-                <?php } ?>    
+                <?php } ?>
             </div>
             <!--  -->
             <!--  -->
@@ -113,50 +114,45 @@
                                                                     <div class="row">
                                                                         <div class="col-md-12 col-xs-12">
                                                                             <?php if ($item['course_banner']) { ?>
-                                                                                <img src="<?php echo AWS_S3_BUCKET_URL.$item['course_banner']; ?>" style="height: 214px !important;" />
-                                                                            <?php } else { ?>   
+                                                                                <img src="<?php echo AWS_S3_BUCKET_URL . $item['course_banner']; ?>" style="height: 214px !important;" />
+                                                                            <?php } else { ?>
                                                                                 <img src="https://automotohrattachments.s3.amazonaws.com/default_course_banner-Uk2W5O.jpg" style="height: 214px !important;" />
-                                                                            <?php } ?>    
+                                                                            <?php } ?>
                                                                         </div>
                                                                     </div>
                                                                     <h1 style="height: 58px;">
-                                                                        <?php 
-                                                                            echo $course['course_title'];
-                                                                            // if ($item['lesson_status'] == "incomplete") {
-                                                                            //     echo "Incomplete Course";
-                                                                            // } else if ($item['lesson_status'] == "completed") {
-                                                                            //     echo "Completed Course";
-                                                                            // }
+                                                                        <?php
+                                                                        echo $course['course_title'];
                                                                         ?>
                                                                     </h1>
                                                                     <br>
                                                                     <div class="row">
                                                                         <div class="col-md-6 col-xs-12">
                                                                             <p class="csColumSection"><strong>COURSE STATUS</strong></p>
-                                                                            
+
                                                                             <?php if ($item['lesson_status'] == "completed") { ?>
                                                                                 <?php if ($item['course_status'] == "passed") { ?>
                                                                                     <p class="text-success">Passed</p>
                                                                                 <?php } else { ?>
                                                                                     <p class="text-danger">Failed</p>
-                                                                                <?php } ?> 
+                                                                                <?php } ?>
                                                                             <?php } else { ?>
-                                                                                <p>Incomplete</p>   
-                                                                            <?php } ?>  
+                                                                                <p>Incomplete</p>
+                                                                            <?php } ?>
                                                                         </div>
                                                                         <div class="col-md-6 col-xs-12">
                                                                             <p class="csColumSection"><strong>LANGUAGE</strong></p>
                                                                             <p>
                                                                                 <?php if ($item['course_language']) { ?>
-                                                                                    <p><?php echo ucfirst($item['course_language']); ?></p>
-                                                                                <?php } else { ?>
-                                                                                    <p >English</p>
-                                                                                <?php } ?> 
-                                                                            </p>
+                                                                            <p><?php echo ucfirst($item['course_language']); ?></p>
+                                                                        <?php } else { ?>
+                                                                            <p>English</p>
+                                                                        <?php } ?>
+                                                                        </p>
                                                                         </div>
                                                                     </div>
 
-                                                                    <hr style="margin-top: 0px !important; color: 000;"> 
+                                                                    <hr style="margin-top: 0px !important; color: 000;">
 
                                                                     <div class="row">
                                                                         <div class="col-md-6 col-xs-12">
@@ -164,7 +160,7 @@
                                                                             <p><?= formatDateToDB($item['created_at'], DB_DATE_WITH_TIME, DATE); ?></p>
                                                                         </div>
                                                                         <div class="col-md-6 col-xs-12">
-                                                                            <p class="csColumSection"><strong>END DATE</strong></p>
+                                                                            <p class="csColumSection"><strong>COMPLETION DATE</strong></p>
                                                                             <p><?= formatDateToDB($item['updated_at'], DB_DATE_WITH_TIME, DATE); ?></p>
                                                                         </div>
                                                                     </div>
@@ -172,23 +168,23 @@
                                                                         <div class="col-md-12 col-xs-12 text-center">
                                                                             <p>&nbsp;</p>
                                                                             <?php if ($page == 'my_courses_history') { ?>
-                                                                                <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/my_history_preview/".$item['sid']); ?>">
+                                                                                <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/my_history_preview/" . $item['sid']); ?>">
                                                                                     <i class="fa fa-eye"></i>
                                                                                     View Content
                                                                                 </a>
                                                                             <?php } else if ($page == 'employee_courses_history') { ?>
                                                                                 <?php if ($reviewAs == 'non_plus') { ?>
-                                                                                    <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/subordinate_history_preview/".$item['sid']."/".$subordinateId); ?>">
+                                                                                    <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/subordinate_history_preview/" . $item['sid'] . "/" . $subordinateId); ?>">
                                                                                         <i class="fa fa-eye"></i>
                                                                                         View Content
                                                                                     </a>
                                                                                 <?php } else if ($reviewAs == 'plus') { ?>
-                                                                                    <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/employee_history_preview/".$item['sid']."/".$subordinateId); ?>">
+                                                                                    <a class="btn btn-info csRadius5 csF16" href="<?php echo base_url("lms/courses/employee_history_preview/" . $item['sid'] . "/" . $subordinateId); ?>">
                                                                                         <i class="fa fa-eye"></i>
                                                                                         View Content
                                                                                     </a>
                                                                                 <?php } ?>
-                                                                            <?php } ?>    
+                                                                            <?php } ?>
                                                                         </div>
                                                                     </div>
                                                                 </article>
@@ -198,13 +194,13 @@
                                                 </div>
                                             </div>
                                             <!-- Course History End -->
-                                        <?php } ?>     
+                                        <?php } ?>
                                     <?php } else { ?>
                                         <div class="row">
                                             <div class="col-sm-12 text-center">
                                                 <h3>No Course History Found!</h3>
                                             </div>
-                                        </div>        
+                                        </div>
                                     <?php } ?>
                                 </div>
                             </div>
@@ -219,7 +215,7 @@
 </div>
 
 
-<script src="https://code.highcharts.com/highcharts.js"></script> 
+<script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
@@ -234,27 +230,27 @@
     var categoriesValues = <?php echo json_encode($categoriesValues); ?>;
 
     Highcharts.chart('historyContainer', {
-		chart: {
-			type: 'bar'
-		},
-		title: {
-			text: 'Course History'
-		},
-		xAxis: {
-			categories: courseCategories,
-			title: {
-				text: 'Courses'
-			}
-		},
-		yAxis: {
-			min: 0,
-			title: {
-				text: 'Course Count'
-			}
-		},
-		series: [{
-			name: 'Course History Count',
-			data: categoriesValues
-		}]
-	});
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Course History'
+        },
+        xAxis: {
+            categories: courseCategories,
+            title: {
+                text: 'Courses'
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Course Count'
+            }
+        },
+        series: [{
+            name: 'Course History Count',
+            data: categoriesValues
+        }]
+    });
 </script>
