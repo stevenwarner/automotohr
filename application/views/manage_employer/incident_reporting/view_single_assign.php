1fb0e573fbd6dd4d2bd1b39200753963d5c50b57
Mubashir Ahmed <mubashar@automotohr.com>
2025-01-11 01:02:31 +0500

    Stopped append of images

--- application/views/manage_employer/incident_reporting/view_single_assign.php
+++ application/views/manage_employer/incident_reporting/view_single_assign.php
@@ -3994,13 +3994,13 @@ $incidentReporterId = $assigned_incidents[0]['reporter_id'];
                         .attr("id", "document-iframe")
                         .attr("class", "uploaded-file-preview")
                         .attr("src", item_url);
-                    $("#document-iframe-holder").append(document_content);
+                    $("#document-iframe-holder").html(document_content);
                 } else {
                     var image_content = $("<img />")
                         .attr("id", "image-tag")
                         .attr("class", "img-responsive")
                         .attr("src", item_url);
-                    $("#document-iframe-holder").append(image_content);
+                    $("#document-iframe-holder").html(image_content);
                 }
 
             } else {
