9fa6bcdb70b9a497a8ec30aaaec51bd5679e92af
Aleem Shaukat <ashaukat@egenienext.com>
2025-02-06 21:06:37 +0500

    go for monday rotuine

--- application/controllers/Cron_lms_courses.php
+++ application/controllers/Cron_lms_courses.php
@@ -32,7 +32,7 @@ class Cron_lms_courses extends CI_Controller
      */
     public function sendCourseReportToManagers()
     {
-        // $this->cron_email_model->sendCourseReportToManagers();
+        $this->cron_email_model->sendCourseReportToManagers();
         $this->cron_email_model->sendDocumentReportToManagers();
     }
 }
