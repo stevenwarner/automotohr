308432894001b2430948af58b0a20e85739fc07e
Aleem Shaukat <ashaukat@egenienext.com>
2025-03-21 17:03:00 +0500

    fix download route

--- application/views/compliance_safety_reporting/edit_incident.php
+++ application/views/compliance_safety_reporting/edit_incident.php
@@ -28,7 +28,7 @@
                     </a>
                 <?php endif; ?>
 
-                <a class="btn btn-green" target="_blank" href="<?= base_url("{$firstSegment}/download_incident/" . $report["sid"]); ?>">
+                <a class="btn btn-green" target="_blank" href="<?= base_url("{$firstSegment}/download_incident/" . $report["csp_reports_sid"]. '/' .$report["sid"]); ?>">
                     <i class="fa fa-download"></i>
                     Download
                 </a>
