880dfb7905ac1f6329ea406579d2fc7ba0230861
Mubashir Ahmed <mubashar@automotohr.com>
2025-05-06 22:12:55 +0500

    Added logo

--- application/views/v1/app/dynamic_pages.php
+++ application/views/v1/app/dynamic_pages.php
@@ -1,12 +1,26 @@
 <main>
     <div class="row">
-        <div class="col-xs-12 column-flex-center background-border-radius top-background-div-service" style="background-image: url('<?= getImageURL($pageContent["pageDetails"]["sourceFile"]); ?>');">
-            <div class="background-image-div-contact-us  ">
-                <h1 class="text-white contact-us-text">
-                    <?= convertToStrip($pageContent["pageDetails"]["heading"]); ?>
-                </h1>
+            <?php if ($this->uri->segment(1) === "market-place-and-integrations"): ?>
+            <div class="col-xs-12 column-flex-center">
+                <br>
+                <br>
+                <img src="<?= getImageURL($pageContent["pageDetails"]["sourceFile"]); ?>" class="img-responsive">
+                 <div class="background-image-div-contact-us  ">
+                    <h1 class="text-white contact-us-text">
+                        <?= convertToStrip($pageContent["pageDetails"]["heading"]); ?>
+                    </h1>
+                </div>
             </div>
-        </div>
+        <?php else: ?>
+            <div class="col-xs-12 column-flex-center background-border-radius top-background-div-service"
+                style="background-image: url('<?= getImageURL($pageContent["pageDetails"]["sourceFile"]); ?>');">
+                <div class="background-image-div-contact-us  ">
+                    <h1 class="text-white contact-us-text">
+                        <?= convertToStrip($pageContent["pageDetails"]["heading"]); ?>
+                    </h1>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
     <div class="row">
         <div class="col-sm-12">
