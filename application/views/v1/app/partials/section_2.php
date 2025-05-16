0662e6de2e5af64838d1309d8966eaaddc077f77
Mubashir Ahmed <mubashar@automotohr.com>
2025-05-16 21:35:37 +0500

    Fixed the issue

--- application/views/v1/app/partials/section_2.php
+++ application/views/v1/app/partials/section_2.php
@@ -85,6 +85,10 @@ $productBg = [
 <?php if ($products) { ?>
     <?php foreach ($products as $key => $product) {
 
+        if (!$product["layout"]) {
+            continue;
+        }
+
 
         //
         $layout = $productBg[$product["layout"]];
@@ -97,78 +101,80 @@ $productBg = [
             $directionP2 = "order-lg-1 order-2";
         }
 
-    ?>
+        ?>
 
-    <?php if($product['status']!='0'){?>
-        <div class="<?= $productBg[$product["layout"]]["bg"]; ?> csSection">
-            <div class="container-fluid">
-                <div class="row d-flex align-items-center justify-content-center">
-                    <div class="col-lg-6 col-12  <?= $directionP2; ?>">
-                        <div class=" position-relative <?= $productBg[$product["layout"]]["image_bg"]; ?>">
-                            <?php if ($layout["images"]) {
-                                foreach ($layout["images"] as $value) {
-                                    echo '<div class="' . $value . '"></div>';
-                                }
-                            } ?>
-                            <?php if ($layout["image_container"]) { ?>
-                                <div class="position-relative">
-                                    <div class="<?= $layout["image_container"][0]; ?>">
-                                        <div class="<?= $layout["image_container"][1] ?>"></div>
+        <?php if ($product['status'] != '0') { ?>
+            <div class="<?= $productBg[$product["layout"]]["bg"]; ?> csSection">
+                <div class="container-fluid">
+                    <div class="row d-flex align-items-center justify-content-center">
+                        <div class="col-lg-6 col-12  <?= $directionP2; ?>">
+                            <div class=" position-relative <?= $productBg[$product["layout"]]["image_bg"]; ?>">
+                                <?php if ($layout["images"]) {
+                                    foreach ($layout["images"] as $value) {
+                                        echo '<div class="' . $value . '"></div>';
+                                    }
+                                } ?>
+                                <?php if ($layout["image_container"]) { ?>
+                                    <div class="position-relative">
+                                        <div class="<?= $layout["image_container"][0]; ?>">
+                                            <div class="<?= $layout["image_container"][1] ?>"></div>
+                                        </div>
+                                    <?php } ?>
+
+                                    <?= getSourceByType(
+                                        $product["sourceType"],
+                                        $product["sourceFile"],
+                                        $layout['props'] ?? ''
+                                    ); ?>
+                                    <?php if ($layout["image_container"]) { ?>
+
                                     </div>
                                 <?php } ?>
-
-                                <?= getSourceByType(
-                                    $product["sourceType"],
-                                    $product["sourceFile"],
-                                    $layout['props'] ?? ''
-                                ); ?>
-                                <?php if ($layout["image_container"]) { ?>
-
-                                </div>
-                            <?php } ?>
-                            <?php if ($productBg[$product["layout"]]["bubbles"]) {
-                                foreach ($productBg[$product["layout"]]["bubbles"] as $bubble) { ?>
-                                    <?php if (is_array($bubble)) { ?>
-                                        <div class="<?= $bubble[0]; ?> d-lg-flex d-none">
-                                            <div class="<?= $bubble[1]; ?>"></div>
-                                        </div>
-                                    <?php } else { ?>
-                                        <div class="<?= $bubble; ?> d-lg-flex d-none"></div>
-                                    <?php } ?>
-                            <?php }
-                            } ?>
+                                <?php if ($productBg[$product["layout"]]["bubbles"]) {
+                                    foreach ($productBg[$product["layout"]]["bubbles"] as $bubble) { ?>
+                                        <?php if (is_array($bubble)) { ?>
+                                            <div class="<?= $bubble[0]; ?> d-lg-flex d-none">
+                                                <div class="<?= $bubble[1]; ?>"></div>
+                                            </div>
+                                        <?php } else { ?>
+                                            <div class="<?= $bubble; ?> d-lg-flex d-none"></div>
+                                        <?php } ?>
+                                    <?php }
+                                } ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-6 col-12 ps-md-3 ps-md-3 ps-lg-5 <?= $directionP1; ?>">
-                        <div class="switch_automoto position-relative ps-0 ps-lg-3 width_90 d-flex flex-column align-items-lg-start align-items-center justify-content-center">
-                            <p class="lightgrey heading-h4-grey opacity-ninety heading">
-                                <?= convertToStrip($product["mainHeading"]); ?>
-                            </p>
-                            <p class="darkgrey mt-3 title text-lg-start text-center">
-                                <?= convertToStrip($product["subHeading"]); ?>
-                            </p>
-                            <p class="lightgrey mt-3 text-lg-start detail text-center opacity-ninety">
-                                <?= convertToStrip($product["details"]); ?>
-                            </p>
-
-                            <a href="<?= main_url($product["buttonLink"]); ?>" class="button jsButtonAnimate explore_btn mt-5 solution-btn d-flex text-white">
-                                <p class="mb-0 btn-text">
-                                    <?= convertToStrip($product["buttonText"]); ?>
+                        <div class="col-lg-6 col-12 ps-md-3 ps-md-3 ps-lg-5 <?= $directionP1; ?>">
+                            <div
+                                class="switch_automoto position-relative ps-0 ps-lg-3 width_90 d-flex flex-column align-items-lg-start align-items-center justify-content-center">
+                                <p class="lightgrey heading-h4-grey opacity-ninety heading">
+                                    <?= convertToStrip($product["mainHeading"]); ?>
                                 </p>
-                                <i class="fa-solid fa-arrow-right top-button-icon ps-3"></i>
-                            </a>
-                        </div>
-                    </div>
+                                <p class="darkgrey mt-3 title text-lg-start text-center">
+                                    <?= convertToStrip($product["subHeading"]); ?>
+                                </p>
+                                <p class="lightgrey mt-3 text-lg-start detail text-center opacity-ninety">
+                                    <?= convertToStrip($product["details"]); ?>
+                                </p>
 
+                                <a href="<?= main_url($product["buttonLink"]); ?>"
+                                    class="button jsButtonAnimate explore_btn mt-5 solution-btn d-flex text-white">
+                                    <p class="mb-0 btn-text">
+                                        <?= convertToStrip($product["buttonText"]); ?>
+                                    </p>
+                                    <i class="fa-solid fa-arrow-right top-button-icon ps-3"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+                <?php if ($layout["outer_bubbles"]) {
+                    foreach ($layout["outer_bubbles"] as $value) {
+                        echo '<div class="' . $value . ' d-lg-block d-none"></div>';
+                    }
+                } ?>
             </div>
-            <?php if ($layout["outer_bubbles"]) {
-                foreach ($layout["outer_bubbles"] as $value) {
-                    echo '<div class="' . $value . ' d-lg-block d-none"></div>';
-                }
-            } ?>
-        </div>
-<?php }?>
+        <?php } ?>
 
 
     <?php } ?>
